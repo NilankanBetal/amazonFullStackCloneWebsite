b80158d0897da001a6df9b2960ae65686fcdfdba
NilankanBetal <nilankanbetal497@gmail.com>
2022-08-07 19:28:22 +0530

    added firebase.js

---
+++ amazon-full-stack-clone-website/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCNAz2v4VUYz7x2yKERtqaXH2_-tbj6KK0",
+    authDomain: "full-stack-clone-9fa91.firebaseapp.com",
+    projectId: "full-stack-clone-9fa91",
+    storageBucket: "full-stack-clone-9fa91.appspot.com",
+    messagingSenderId: "762291337896",
+    appId: "1:762291337896:web:3c6e78d4bd94c45098a09d",
+    measurementId: "G-SNS76H9SY5"
+  };
